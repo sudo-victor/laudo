adc255e2f8b8f4642ba6b62cbb06dcb674c82369
Victor de Souto Soares <vitor.soares@lojainterativa.com>
2022-02-11 10:09:40 -0300

    chore: fix the add equipment action and handle submit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const laudoFormElement = document.querySelector('#laudo-form');;
 const cabecalhoElement = document.querySelector('#cabecalho');
 const inputsHiddenElement = document.querySelector('.inputs-hidden');
 const equipmentsElement = document.querySelector('#equipments');
@@ -11,11 +12,14 @@ function toggleInputsHidden(e) {
 }
 
 function addEquipmentGroup() {
+    const newElement = document.createElement('div');
+    newElement.classList.add('row'); 
+    newElement.classList.add('grupo-equipamentos');
+    
     const newEquipment = `
-    <div class="row grupo-equipamentos">
         <div class="form-group col">
             <label for="equipamento">Nome do Equipamento:</label>
-            <select class="form-control form-control-sm" id="equipamento[]">
+            <select class="form-control form-control-sm equipamento" id="equipamento">
                 <option value="1">Selecione um equipamento</option>
                 <option value="1">2</option>
                 <option value="1">3</option>
@@ -26,23 +30,24 @@ function addEquipmentGroup() {
 
         <div class="form-group col">
             <label for="numero_rastreio">N° de Rastreio:</label>
-            <input class="form-control form-control-sm" id="numero_rastreio[]" aria-describedby="numeroRastreioField">
+            <input class="form-control form-control-sm numero_rastreio" id="numero_rastreio" aria-describedby="numeroRastreioField">
         </div>
 
         <div class="form-group col">
             <label for="numero_serie_ca">N° de Séria/CA:</label>
-            <input class="form-control form-control-sm" id="numero_serie_ca[]" aria-describedby="numeroSerieCAField">
+            <input class="form-control form-control-sm numero_serie_ca" id="numero_serie_ca" aria-describedby="numeroSerieCAField">
         </div>
 
         <div class="form-group col">
             <label for="resultado">Resultado:</label>
-            <input class="form-control form-control-sm" id="resultado[]" aria-describedby="resultadoField">
+            <input class="form-control form-control-sm resultado" id="resultado" aria-describedby="resultadoField">
         </div>
-    </div>
 
     `
 
-    equipmentsElement.innerHTML += newEquipment;
+    newElement.innerHTML = newEquipment;
+
+    equipmentsElement.appendChild(newElement);
 }
 
 function removeEquipmentGroup() {
@@ -54,4 +59,56 @@ function removeEquipmentGroup() {
     equipmentsElement.removeChild(equipmentGroupElement);
 }
 
+function handleSubmit(e) {
+    e.preventDefault();
+
+    let data = {}
+
+    data['laudo'] = document.querySelector('#laudo_id').value;
+    data['data_ensaio'] = document.querySelector('#data_ensaio').value;
+    data['testador'] = document.querySelector('#testador').value;
+    data['placa_matricula'] = document.querySelector('#placa_matricula').value;
+    data['cabecalho'] = document.querySelector('#cabecalho').value;
+    data['assinatura_1'] = document.querySelector('#assinatura_1').value;
+    data['assinatura_2'] = document.querySelector('#assinatura_2').value;
+    data['ensaios_realizados'] = document.querySelector('#ensaios_realizados').value;
+
+    if(Number(data.cabecalho) === 0) {
+        data['categoria'] = document.querySelector('#categoria').value;
+        data['estado'] = document.querySelector('#estado').value;
+        data['numero_serie'] = document.querySelector('#numero_serie').value;
+        data['modelo'] = document.querySelector('#modelo').value;
+        data['chassi'] = document.querySelector('#chassi').value;
+        data['cesto'] = document.querySelector('#cesto').value;
+        data['estabilizador'] = document.querySelector('#estabilizador').value;
+    }
+    
+    data['nome_cliente'] = document.querySelector('#nome_cliente').value;
+
+    let equipments = []
+
+    // Form Array
+    const equipamentoArrayGroup = document.querySelectorAll('.equipamento');
+    const numeroRastreioArrayGroup = document.querySelectorAll('.numero_rastreio');
+    const numeroSerieArrayGroup = document.querySelectorAll('.numero_serie_ca');
+    const resultadoArrayGroup = document.querySelectorAll('.resultado');
+
+    equipamentoArrayGroup.forEach((_, idx) => {
+        const equipmentData = {
+            equipamento: equipamentoArrayGroup[idx].value,
+            numero_rastreio: numeroRastreioArrayGroup[idx].value,
+            numero_serie: numeroSerieArrayGroup[idx].value,
+            resultado: resultadoArrayGroup[idx].value
+        }
+
+        equipments.push(equipmentData);
+    })
+
+    data['equipamentos'] = [...equipments];
+
+    console.log(data);
+    
+}
+
+laudoFormElement.addEventListener('submit', handleSubmit)
 cabecalhoElement.addEventListener('change', toggleInputsHidden);
